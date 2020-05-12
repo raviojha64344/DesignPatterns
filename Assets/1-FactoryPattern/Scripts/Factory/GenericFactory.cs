5787279b784ea8d580daaa13f2ffbbd29a0e6d7f
Ravi Oza <ravi.oza@digitas.com>
2020-05-12 21:17:36 +0530

    first commit

---
+++ Assets/1-FactoryPattern/Scripts/Factory/GenericFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPattern.FactoryPattern
+{
+    /*
+     * Generic Factory Class
+     */
+    public class GenericFactory<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        [SerializeField]
+        private T prefab;
+
+        public T GetInstance()
+        {
+            return Instantiate(prefab) as T;
+        }
+
+        public T GetInstance(Transform parent)
+        {
+            return Instantiate(prefab, parent) as T;
+        }
+
+        public T GetInstance(Vector3 position, Quaternion rotation)
+        {
+            return Instantiate(prefab, position, rotation) as T;
+        }
+    }
+}
